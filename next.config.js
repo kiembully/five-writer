58177f3ce41dc0b8c0ea86a4c793f9295acee79a
kiembully <54204618+kiembully@users.noreply.github.com>
2021-11-23 20:26:15 +0800

    asd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
   reactStrictMode: true,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-}
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  }
+})
