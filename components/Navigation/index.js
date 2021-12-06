8773b989ca2f4fc1870d633f761386ee06478cd1
kiembully <54204618+kiembully@users.noreply.github.com>
2021-12-06 17:28:04 +0800

    Update index.js

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -6,7 +6,7 @@ import DashboardNav from "./DashboardNav";
 const Navigation = () => {
     const router = useRouter();
     function handleNavigation() {
-        const guestPages = ['/','/login', '/register', '/forgot'];
+        const guestPages = ['/','/login', '/register', '/forgot', '/404'];
         return guestPages.includes(router.pathname)
     }
 
